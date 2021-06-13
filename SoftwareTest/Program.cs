6f0cc9c02cefed2055d82b92d5aa2da3867d8da4
Milad Esfandiyari <miladisjobs@gmail.com>
2021-06-13 09:17:53 +0200

    add source files and fluent assertions test

---
+++ SoftwareTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SoftwareTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+        public static int Add (int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
